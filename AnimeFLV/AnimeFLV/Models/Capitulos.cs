fd6841cf4a4e07f6c7cc309d73de33570f975606
BorisSalazar92 <borismancia2007@gmail.com>
2020-10-16 23:14:24 -0600

    Avance pantalla series y  modelo capitulos

---
+++ AnimeFLV/AnimeFLV/Models/Capitulos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnimeFLV.Models
+{
+    public class Capitulos
+    {
+        public int ID { get; set; }
+        public string SerieId { get; set; }
+        public string Name { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ AnimeFLV/AnimeFLV/Models/Series.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnimeFLV.Models
+{
+    public class Series
+    {
+        public int ID { get; set; }
+        public string CategoriaId { get; set; }
+        public string Name { get; set; }
+        public string synopsis { get; set; }
+        public string ImagePath { get; set; }
+        
+    }
+}
